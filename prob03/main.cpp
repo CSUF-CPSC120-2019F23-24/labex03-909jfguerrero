0ed09162b0b099bb487f6efbdba780edfecb33dd
909jfguerrero <jfguerrero@csu.fullerton.edu>
2019-09-07 14:27:44 -0700

    finished prob03

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,31 @@
 // This program calculates a person's height in feet (') and inches (").
+// This program calculates the amount of paint needed.
+
+#include <iostream>
+
+int main()
+{
+  double inches, feet, height;
+  // get height in feet
+  std::cout << "what is the height in feet?";
+  std::cin >> feet;
+
+  //calculate feet to inches
+  inches = feet * 12;
+
+  //display the inches
+  std::cout << "The height in \"inches\" is " << inches << '\n';
+
+  //get the height in inches
+  std::cout << "what is the height in inches?";
+  std::cin >> inches;
+
+  //calculate inches to feet
+  feet = inches /12;
+
+  //display the feet
+  std::cout << "The height in \"feet\" is " << feet << '\n';
+
+
+  return 0;
+}
